9f0175371848aee2cabc4cb87ea28a8720c4ae5f
misprachi023 <prachimishra23123@gmail.com>
2024-02-11 00:27:20 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
-				<Route path="/notes" element={<Notes />} />
+				<Route path="/posts" element={<Notes />} />
 			</Routes>
 		</>
 	);
