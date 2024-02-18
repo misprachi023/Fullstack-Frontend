57df7a39cea6160065265d328ebc193db8657113
Prachi Mishra <143651644+misprachi023@users.noreply.github.com>
2024-02-18 13:57:17 +0530

    Update notes.jsx

--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -1,46 +1,56 @@
-import React, { useEffect } from 'react'
-import {Box, Button, Heading } from '@chakra-ui/react'
+import React, { useEffect , useContext	} from "react";
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+import {useNavigate} from "react-router-dom"
+
 
 /**
-* @author
-* @function Notes
-**/
+ * @author
+ * @function Notes
+ **/
 
 const Notes = (props) => {
-  const [notes,setNotes]=React.useState([])
+	
+	
+	const {isLogin,setIsLogin}=useContext(AuthContext)
+	const [notes, setNotes] = React.useState([]);
+
+	const navigate=useNavigate();
 
 
-  useEffect(()=>{
-    const getData=async()=>{
-      try{
-        const res=await fetch("https://tiny-cyan-squid-gown.cyclic.app/posts",{
-          method:"GET",
-          mod:"cors",
-          credentials:"include"
-        })
-        const data= await res.json();
-        console.log(data);
-        setNotes(data)
-      }
-      catch(err){
-        console.log(err);
-      }
-    }
-    getData();
-  },[])
-  return(
-    <>
-      <h1>posts</h1>
-      {console.log(notes)}
-    {notes.map((item)=>{
-      <Box border={'1px solid black'} >
-        <Heading>{item.title}</Heading>
-        <Heading>{item.body}</Heading>
-        <p>{item.device}</p>
-      </Box>
-    })}
-    </>
-  
-   )
-  }
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const res = await fetch("https://tiny-cyan-squid-gown.cyclic.app/posts",{
+					method: "GET",
+					credentials: "include",
+				});
+				const data = await res.json();
+				console.log(data);
+				setNotes(data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		getData();
+	}, []);
+	return (
+		<>
+			
+			
+			
+			<Box width={"80%"} margin={"auto"}>
+			<Heading>Notes</Heading>	
+				{notes.map((item) => (
+					<Box border={"1px solid black"} width={"30%"} margin={"auto"} key={item._id}>
+						<Box>{item.title}</Box>
+						<Box>{item.body}</Box>
+						<Box>{item.device}</Box>
+					</Box>
+				))}
+			</Box>
+		</>
+	);
+};
 export default Notes;
